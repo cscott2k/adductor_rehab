0bee1b1eeff3914022faa9bfad686c8c89ab1c81
cscott2k <craig.scott@gmail.com>
2026-04-19 20:56:23 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'adductor-rehab-v2';
+const CACHE = 'adductor-rehab-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
